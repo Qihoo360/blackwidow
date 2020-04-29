2eca7883a25a430dde52a7d6c4d256b826a3d1e4
Ted Mosly <wanghenshui@qq.com>
2020-04-29 19:17:21 +0800

    cur_meta_version_ and cur_meta_timestamp_ uninit fix (#9)

--- src/zsets_filter.h
+++ src/zsets_filter.h
@@ -23,7 +23,11 @@ class ZSetsScoreFilter : public rocksdb::CompactionFilter {
  public:
   ZSetsScoreFilter(rocksdb::DB* db,
                    std::vector<rocksdb::ColumnFamilyHandle*>* handles_ptr) :
-    db_(db), cf_handles_ptr_(handles_ptr), meta_not_found_(false) {}
+    db_(db),
+    cf_handles_ptr_(handles_ptr),
+    meta_not_found_(false),
+    cur_meta_version_(0),
+    cur_meta_timestamp_(0) {}
 
   bool Filter(int level, const rocksdb::Slice& key,
               const rocksdb::Slice& value,
